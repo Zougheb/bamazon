2bd5fc2774cd6a83c30deed30f8fff1b0fc4875c
mahmoud zougheb <mahmoudzougheb@calvisitor-10-105-129-60.calvisitor.1918.berkeley.edu>
2017-10-23 19:24:50 -0700

    changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,17 +23,6 @@ connection.query('SELECT * FROM Bamazon_db.products;',function(err,res){
 	console.log("-----------------------------------");
 });
 
-//2 messages
-
-//First message = ID of the product they would like to buy
-
-//Second message = how many of the product they would like to buy
-
-// Check if your store has enough quantity of the product to meet the customer's request. If not, you should respond to the user by saying: "Insufficient quantity" and prevent the order from going through.
-
-// If your store DOES have enough of the product to meet the customer's request, you should fulfill their order. This means that you should show them the total cost of their puchase. Then update the SQL database to reflect the remaining quantity.
-
-//The following is from class to use for this assignment
 
 inquirer.prompt([{
    type: "list",
