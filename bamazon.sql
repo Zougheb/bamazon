7ec9840e95f3db586595ad4a01be6886e055e6b5
mahmoud zougheb <mahmoudzougheb@calvisitor-10-105-129-60.calvisitor.1918.berkeley.edu>
2017-10-23 20:03:38 -0700

    changes

--- bamazon.sql
+++ bamazon.sql
@@ -13,37 +13,34 @@ create table products (
 
 
 INSERT INTO products
-VALUES (01, "Medal", "Accesories", 89.99, 15)
+VALUES (01, "Medal", "Accesories", 89.99, 15);
 
 INSERT INTO products
-VALUES (02, "Hose", "crafts", 12, 39)
+VALUES (02, "Hose", "crafts", 12, 39);
 
 INSERT INTO products
-VALUES (03, "The Catcher in the Rye", "books", 14.99, 59)
+VALUES (04, "Cutter", "office", 0.99, 380);
 
 INSERT INTO products
-VALUES (04, "Cutter", "office", 0.99, 380)
+VALUES (05, "i-Phone", "electronics", 799.00, 3);
 
 INSERT INTO products
-VALUES (05, "i-Phone", "electronics", 799.00, 3)
+VALUES (06, "Go Pro Camera", "electronics", 599, 48);
 
 INSERT INTO products
-VALUES (06, "Go Pro Camera", "electronics", 599, 48)
+VALUES (07, "Play Station4", "electronics", 199, 38);
 
 INSERT INTO products
-VALUES (07, "Play Station4", "electronics", 199, 38)
+VALUES (08, "Printer", "Stationary", 16.99, 38);
 
 INSERT INTO products
-VALUES (08, "Printer", "Stationary", 16.99, 38)
+VALUES (09, "Wooden spindle", "crafts", 3.99, 69);
 
 INSERT INTO products
-VALUES (09, "Wooden spindle", "crafts", 3.99, 69)
+VALUES (10, "Tshirt", "Men", 980.00, 3);
 
 INSERT INTO products
-VALUES (10, "Tshirt", "Men", 980.00, 3)
+VALUES (11, "Shoes", "Men", 25, 94);
 
 INSERT INTO products
-VALUES (11, "Shoes", "Men", 25, 94)
-
-INSERT INTO products
-VALUES (12, "Pens", "office", 3.99, 93)
+VALUES (12, "Pens", "office", 3.99, 93);
